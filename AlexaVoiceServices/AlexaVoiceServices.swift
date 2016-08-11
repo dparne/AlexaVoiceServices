433285a7e7168c6ed5678aad01c21fbde87cd38b
Dinesh Reddy Parne <dineshparne@gmail.com>
2016-08-11 10:34:28 -0500

    Signal and observer setup

--- AlexaVoiceServices/AlexaVoiceServices.swift
+++ AlexaVoiceServices/AlexaVoiceServices.swift
@@ -10,34 +10,41 @@ import Foundation
 import ReactiveCocoa
 import Result
 
-class AlexaVoiceServices: AlexaVoiceServicesProtocol {
+public class AlexaVoiceServices: AlexaVoiceServicesProtocol {
  
     private var (_eventSignal,_eventListener) = Signal<AlexaVoiceServicesEvent, NoError>.pipe()
     private var (_directiveProvider, _directiveObserver) = Signal<AlexaVoiceServicesDirective, NoError>.pipe()
     private let config: AlexaVoiceServicesConfiguration
+    private let apiCommunicator: AlexaVoiceServicesApiCommunicator
     
-    var eventListener: Observer<AlexaVoiceServicesEvent, NoError> {
+    public var eventListener: Observer<AlexaVoiceServicesEvent, NoError> {
         get {
             return _eventListener
         }
     }
     
-    var directiveProvider: Signal<AlexaVoiceServicesDirective, NoError> {
+    public var directiveProvider: Signal<AlexaVoiceServicesDirective, NoError> {
         get {
             return _directiveProvider
         }
     }
     
-    init(config: AlexaVoiceServicesConfiguration) {
+    public init(config: AlexaVoiceServicesConfiguration,
+         apiCommunicator: AlexaVoiceServicesApiCommunicator) {
         //1st phase of init
         self.config = config
+        self.apiCommunicator = apiCommunicator
         //2nd phase of init
         setupEventSignal()
     }
     
     func setupEventSignal() {
-        _eventSignal.observeNext({ avsEvent in
-            
+        _eventSignal.observeNext({[weak self] avsEvent in
+            self?.apiCommunicator.send(avsEvent).startWithSignal({ (signal, disposable) in
+                signal.observeNext({[weak self] (directive) in
+                    self?._directiveObserver.sendNext(directive)
+                })
+            })
         })
     }
 }
